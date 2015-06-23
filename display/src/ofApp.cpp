4e832be8a22e1f075aa4ed900024f847d760b060
Ross Cairns <ross@electricglen.com>
2015-06-23 10:11:12 +0100

    Minor refactors

--- display/src/ofApp.cpp
+++ display/src/ofApp.cpp
@@ -6,8 +6,8 @@
 
 class Expires {
 public:
-    Expires():ttl(1.0f) { time=ofGetElapsedTimef(); };
-    Expires(float ttl):ttl(ttl) { time=ofGetElapsedTimef(); };
+    Expires():ttl(1.0f) { time=0; };
+    Expires(float ttl):ttl(ttl) { time=0; };
     bool hasExpired() { return ofGetElapsedTimef() - time > ttl; };
     void reset() { time=ofGetElapsedTimef(); }
     // Returns true if expired
@@ -17,17 +17,17 @@ private:
     float time;
 };
 
-inline float scaleToBounds ( float width, float height, float boundsWidth, float boundsHeight ) {
+inline float scaleToBoundsRatio ( float width, float height, float boundsWidth, float boundsHeight ) {
     float widthScale = boundsWidth / width;
     float heightScale = boundsHeight / height;
     return min( widthScale, heightScale );
 }
 
-inline string getNextFilename( const vector<ofFile> &files, const string currentFileName ) {
+inline string getNextFilenameInFiles( const vector<ofFile> &files, const string currentFilename ) {
     if ( files.size() == 0 ) { return ""; }
     int idx = 0;
     for ( ; idx < files.size(); ++idx ) {
-        if ( files[ idx ].getFileName() == currentFileName ) {
+        if ( files[ idx ].getFileName() == currentFilename ) {
             break;
         }
     }
@@ -70,15 +70,16 @@ void ofApp::update () {
     // How often we check for new files
     static Expires reloadDirTime( 2.0f );
     
-    // Check for a new file
+    // Check any changes in the video files folder
     if ( reloadDirTime.resetIfExpired() ) {
         
         ofDirectory d;
         d.listDir( SELFIES_DISPLAY_VIDEO_DIR );
-        
         vector<ofFile> incomingVideoFiles = d.getFiles();
         
         // Look for lock file
+        // If a file named 'lock' exists the capture app is
+        // still writing the file to disk, so just ignore
         bool hasLockFile = false;
         for ( vector<ofFile>::iterator it = incomingVideoFiles.begin(); it != incomingVideoFiles.end(); ++it ) {
             if ( it->getFileName() == "lock" ) {
@@ -87,27 +88,31 @@ void ofApp::update () {
             }
         }
         if ( !hasLockFile ) {
+            // Find if any files have changed
             vector<ofFile> differentFiles;
             set_symmetric_difference( videoFiles.begin(), videoFiles.end(), incomingVideoFiles.begin(), incomingVideoFiles.end(), back_inserter( differentFiles ) );
-            
+            // If they have, take the newest video (by dated filename)
             if ( differentFiles.size() > 0 ) {
                 videoFiles = incomingVideoFiles;
                 playVideo( *this, differentFiles.back().getFileName() );
             }
         }
     }
-    // If the current file is finished
+    // If the current file is finished, queue the next filename
     else {
         if ( videoPlayer.getIsMovieDone() && videoFiles.size() > 0 ) {
-            playVideo( *this, getNextFilename( videoFiles, currentVideoFilename ) );
+            playVideo( *this, getNextFilenameInFiles( videoFiles, currentVideoFilename ) );
         }
     }
     // if the next button had been released
     // if the previous button has been released
     
-    // Play any videos
+    // Play any videos which have need queued up
+    // by first closing the video player, then
+    // once it has closed (later) load the next
     if ( nextUpVideoPath != "" ) {
         if ( videoPlayer.isLoaded() ) {
+            videoPlayer.stop();
             videoPlayer.closeMovie();
         } else {
             ofLogNotice() << "Will load: " << nextUpVideoPath;
@@ -120,9 +125,11 @@ void ofApp::update () {
 
 void ofApp::draw () {
     ofClear( ofColor::black );
+    
+    // Draw the video scaled to fit the window
     ofTranslate( ofGetWidth() / 2.0f, ofGetHeight() / 2.0f );
-    float scaleToFit = scaleToBounds( videoPlayer.getWidth(), videoPlayer.getHeight(), ofGetWidth(), ofGetHeight() );
-    ofScale( scaleToFit, scaleToFit, 1.0f );
+    float scaleToFitRatio = scaleToBoundsRatio( videoPlayer.getWidth(), videoPlayer.getHeight(), ofGetWidth(), ofGetHeight() );
+    ofScale( scaleToFitRatio, scaleToFitRatio, 1.0f );
     ofTranslate( -videoPlayer.getWidth() / 2.0f, -videoPlayer.getHeight() / 2.0f );
     videoPlayer.draw( 0, 0 );
 }
