a4b99cea10525fd025365759091fa93cb723962d
Ross Cairns <ross@electricglen.com>
2015-07-08 16:36:34 +0100

    Fixed case of filename

--- display/src/ofApp.h
+++ display/src/ofApp.h
@@ -5,7 +5,7 @@
 #define SELFIES_DISPLAY_HEIGHT 1366
 #define SELFIES_DISPLAY_WIDTH 768
 #define SELFIES_DISPLAY_VIDEO_DIR "./videos/"
-#define SELFIES_CAPTURE_NEW_VIDEO_IMAGE "newVideo.png"
+#define SELFIES_CAPTURE_NEW_VIDEO_IMAGE "NewVideo.png"
 
 class ofApp : public ofBaseApp {
  public:
