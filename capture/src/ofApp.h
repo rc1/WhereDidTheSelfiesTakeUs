2dccd8fa509a4c2d283e6cc76d22b0483e04c7f3
Ross Cairns <ross@electricglen.com>
2015-07-08 15:28:01 +0100

    Fixed delay time

--- capture/src/ofApp.h
+++ capture/src/ofApp.h
@@ -16,28 +16,28 @@
 #define SELFIES_CAPTURE_TARGET_DIR "../../../display/bin/data/videos/"
 #define SELFIES_CAPTURE_HEIGHT 460
 #define SELFIES_CAPTURE_WIDTH 800
-#define SELFIES_CAPTURE_THROTTLE_SEC 0.5f
+#define SELFIES_CAPTURE_THROTTLE_SEC 2.0f
 #define SELFIES_CAPTURE_DIGIT_IMAGE "CounterDigits.png"
 #define SELFIES_CAPTURE_SAVING_IMAGE "Saving.png"
 
 class ofApp : public ofBaseApp {
- public:
+public:
     void setup ();
     void exit ();
     void update ();
     void draw ();
-		
+    
     void keyPressed ( int key );
     void keyReleased ( int key );
     void windowResized ( int w, int h);
     void gotMessage ( ofMessage msg );
-
-    #ifdef TARGET_LINUX_ARM
+    
+#ifdef TARGET_LINUX_ARM
     ofxRPiCameraVideoGrabber videoGrabber;
     OMXCameraSettings omxCameraSettings;
-    #else
+#else
     ofVideoGrabber videoGrabber;
-    #endif
+#endif
     
     OnionSkinRingBufferSettings onionSkinSettings;
     OnionSkinRingBuffer onionskin;
@@ -51,11 +51,11 @@ class ofApp : public ofBaseApp {
 #ifdef TARGET_LINUX_ARM
     ofxThreadedSavingImage saveImage;
 #endif
-
+    
 #ifdef TARGET_OSX
     ThreadedImageSaver imageSaver;
 #endif
-
+    
     bool shouldCaptureFrame;
     bool shouldDrawOnionSkin;
     bool shouldCreateRecording;
