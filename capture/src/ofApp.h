96ab398d2f373e0c8186a898eefe7cae1715d490
Ross Cairns <ross@electricglen.com>
2015-07-08 19:42:32 +0100

    Added fadeTime platform dependant

--- capture/src/ofApp.h
+++ capture/src/ofApp.h
@@ -16,7 +16,11 @@
 // #define SELFIES_CAPTURE_TARGET_DIR "../../../display/bin/data/videos/"
 #define SELFIES_CAPTURE_HEIGHT 460
 #define SELFIES_CAPTURE_WIDTH 800
-#define SELFIES_CAPTURE_THROTTLE_SEC 0.3f
+#ifdef TARGET_OSX
+    #define SELFIES_CAPTURE_THROTTLE_SEC 0.3f
+#else
+    #define SELFIES_CAPTURE_THROTTLE_SEC 2.0f
+#endif
 #define SELFIES_CAPTURE_DIGIT_IMAGE "CounterDigits.png"
 #define SELFIES_CAPTURE_SAVING_IMAGE "Saving.png"
 
